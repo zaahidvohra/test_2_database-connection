5a5d1f33a9e2618a3165bebf263adc5998dd1db3
HP <HP@ZAAHID-VICTUS>
2023-01-20 10:48:25 +0530

    Add project files.

---
+++ test_2/WebForm1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="test_2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 29px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td class="auto-style1">Name</td>
+                    <td class="auto-style1">
+                        <asp:TextBox ID="name_txt" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Department</td>
+                    <td>
+                        <asp:TextBox ID="dep_txt" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>PRN</td>
+                    <td>
+                        <asp:TextBox ID="prn_txt" runat="server"></asp:TextBox></td>
+                </tr>
+            </table>
+        </div>
+        <asp:Button ID="view_btn" runat="server" Text="View" OnClick="view_btn_Click" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="insert_btn" runat="server" Text="Insert" OnClick="insert_btn_Click" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="delete_btn" runat="server" Text="Delete" OnClick="delete_btn_Click" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="update_btn" runat="server" Text="Update" />
+
+        <asp:GridView ID="Gridview1" runat="server"></asp:GridView>
+    </form>
+    
+</body>
+</html>
---
+++ test_2/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace test_2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\HP\Desktop\Sem 4\Dot Net\Login Form (Test_1)\test_2\test_2\App_Data\Database1.mdf"";Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+        }
+
+        public void display()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from form";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            Gridview1.DataSource= dt;
+            Gridview1.DataBind();
+        }
+
+        protected void view_btn_Click(object sender, EventArgs e)
+        {
+            display();
+            Response.Write("View button clicked");
+        }
+
+        protected void insert_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into form values('"+name_txt.Text+"','"+dep_txt.Text+"','"+prn_txt.Text+"')";
+            cmd.ExecuteNonQuery();
+            name_txt.Text = "";
+            dep_txt.Text = "";
+            prn_txt.Text = "";
+            display();
+        }
+
+        protected void delete_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from form where Name='" + name_txt.Text + "'";
+            cmd.ExecuteNonQuery();
+            name_txt.Text = "";
+            display();
+        }
+    }
+}
